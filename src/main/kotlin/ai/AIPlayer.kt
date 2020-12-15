e80e3538929656119f412f646e85fae530328dfd
Haniye Mollaei <HaniyeMolaei1378@gmail.com>
2020-12-15 03:33:14 +0330

    AIPlayer_search

--- src/main/kotlin/ai/AIPlayer.kt
+++ src/main/kotlin/ai/AIPlayer.kt
@@ -1,37 +1,63 @@
 package ai
 
 import game.*
+import java.lang.Integer.max
+import java.lang.Integer.min
 
 class AIPlayer {
 
+    val boardCalculator = BoardCalculator()
+    val boardManager = BoardManager(Game())
 
     // Initial values of
-    // Aplha and Beta
+    // Alpha and Beta
     var MAX = 1000
     var MIN = -1000
     var depthCount : Int = 0
 
-    fun search(state: Array<Array<Side>>): Cell {
-        return Cell(0, 0)
+    fun search(state: Array<Array<Side?>>): Cell {
+
+        val stateCopy = state
+        val defaultDepth = 6
+        var bestScore = MIN
+        lateinit var bestMove : Cell
+
+        val availableMoves =boardCalculator.availableCells(state , Side.BLACK)
+        availableMoves.forEach {
+
+            //??need new initial state?
+            boardManager.putDisk(it.x , it.y , availableMoves)
+
+            val moveSore = minValue(state , MIN , MAX , defaultDepth)
+            if(bestScore<moveSore){
+                bestScore = moveSore
+                bestMove=it
+            }
+
+
+        }
+
+        return bestMove
     }
 
     fun maxValue(state: Array<Array<Side?>>, a: Int, b: Int, depth: Int): Int {
-        if (isInTerminalState(state) || depth == depthCount){
+        if (isInTerminalState(state) || depth == this.depthCount){
             return getUtility(state)
         }
-        depthCount++
-        var boardCalculator = BoardCalculator()
-        var boardManager = BoardManager(Game())
+        this.depthCount++
 
-        var maxValue = MIN
+        var maxVal = MIN
         val availableMoves =boardCalculator.availableCells(state , Side.BLACK)
         availableMoves.forEach{
             boardManager.putDisk(it.x , it.y , availableMoves)
-            if (maxValue < minValue(state , a , b , depth)){
-                maxValue = minValue(state, a , b , depth)
+            val mn = minValue(state , a , b , depth)
+            maxVal = max(maxVal , mn)
+ //         a = max(a , mn)
+            if (a <= b){
+                return maxVal
             }
         }
-        return maxValue
+        return maxVal
     }
 
     fun minValue(state: Array<Array<Side?>>, a: Int, b: Int, depth: Int): Int {
@@ -40,18 +66,23 @@ class AIPlayer {
             return getUtility(state)
         }
         depthCount++
-        var boardCalculator = BoardCalculator()
-        var boardManager = BoardManager(Game())
 
-        var minValue = MAX
+        var minVal = MAX
         val availableMoves =boardCalculator.availableCells(state , Side.WHITE)
         availableMoves.forEach{
             boardManager.putDisk(it.x , it.y , availableMoves)
-            if (minValue > maxValue(state , a , b , depth)){
-                minValue = maxValue(state, a , b , depth)
+
+            val mx = maxValue(state , a , b , depth)
+            minVal = min(minVal , mx)
+
+//            if(b > mx){
+//                b = mx
+//            }
+            if (b <= a){
+                return minVal
             }
         }
-        return minValue
+        return minVal
     }
 
     fun isInTerminalState(state: Array<Array<Side?>>): Boolean {
@@ -61,7 +92,4 @@ class AIPlayer {
     fun getUtility(state: Array<Array<Side?>>): Int {
         return 0
     }
-
-    
-
 }
