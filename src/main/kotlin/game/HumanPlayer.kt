88c2ad82fa9837ec39af576de88e28a3ed129a79
Mehdi <mehditeymorian322@gmail.com>
2020-12-17 17:29:30 +0330

    correct input format for choosing a cell

--- src/main/kotlin/game/HumanPlayer.kt
+++ src/main/kotlin/game/HumanPlayer.kt
@@ -6,12 +6,12 @@ class HumanPlayer(turn: Side) : Player(turn) {
 
     override fun move(state: Array<Array<Side?>>, availableCells: List<Cell>): Cell {
         val line = readLine()?.split(" ")
-        return if (line == null) {
+        return if (line == null || line.size != 2) {
             println("Wrong Input, try again")
             move(state, availableCells)
         } else {
-            val x = line[0].toInt()
-            val y = line[1].toInt()
+            val x = line[0].toInt() - 1
+            val y = line[1].toUpperCase()[0] - 'A'
             Cell(x,y)
         }
     }
