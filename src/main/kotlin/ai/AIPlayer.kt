75c74be05b50d42d596c4114d8cff54ddb97026f
Mehdi <mehditeymorian322@gmail.com>
2020-12-18 17:37:35 +0330

    Refactor AIPlayer

--- src/main/kotlin/ai/AIPlayer.kt
+++ src/main/kotlin/ai/AIPlayer.kt
@@ -4,122 +4,94 @@ import game.*
 import kotlin.math.max
 import kotlin.math.min
 
+private const val MAX_DEPTH = 20
+
 class AIPlayer(turn: Side) : Player(turn) {
     private val boardCalculator = BoardCalculator()
     private val featureWeights = DoubleArray(FEATURES_COUNT) { 1.0 }
     private val utilityCalculator = UtilityCalculator(boardCalculator, featureWeights)
-    private var searchDepth: Int = 20
-    private var depthCount: Int = 0
-
-//    init {
-//        featureWeights[5] = 5.0
-//    }
-
 
 
     override fun move(state: Array<Array<Side?>>, availableCells: List<Cell>): Cell {
 
-        var bestMove = availableCells[0]
+//        var bestMove = availableCells[0]
+//        var bestPoint = Double.MIN_VALUE
+//        availableCells.forEach {
+//            val utility = utilityCalculator.calculate(state, it, turn)
+//            if (utility > bestPoint) {
+//                bestMove = it
+//                bestPoint = utility
+//            }
+//        }
+
+        val boundary = doubleArrayOf(Double.MIN_VALUE, Double.MAX_VALUE)// alpha beta
         var bestPoint = Double.MIN_VALUE
+        var bestMove = Cell(-1, -1)
         availableCells.forEach {
-            val utility = utilityCalculator.calculate(state, it, turn)
-            if (utility > bestPoint) {
+            val each = maxValue(boardCalculator.copy(state), boundary, it, playerTurn, 0)
+            if (each > bestPoint) {
+                bestPoint = each
                 bestMove = it
-                bestPoint = utility
             }
         }
 
-//        var boundary: Array<Double> = arrayOf(Double.MAX_VALUE, Double.MIN_VALUE)
-//        var bestMove = availableCells[0]
-//        var bestPoint = boundary[1]
-//        availableCells.forEach {
-//            depthCount = 0 ;
-//            val currentPoint = minValue(boardCalculator.copy(state) , boundary , it , turn)
-//            if (currentPoint > bestPoint) {
-//                bestMove = it
-//                bestPoint = currentPoint
-//                println(" best move : ${bestMove.x} , ${bestMove.y}     || best point : $bestPoint")
-//            }
-//        }
         return bestMove
     }
 
 
-
-    fun maxValue(
+    private fun maxValue(
         state: Array<Array<Side?>>,
-        boundary: Array<Double>,
+        boundary: DoubleArray,
         cell: Cell,
-        turn_max: Side
+        turn: Side,
+        depth: Int
     ): Double {
 
-        var Opponent: Side = turn_max.flip()
+        if (isTerminalState(state, depth))
+            return utilityCalculator.calculate(state, cell, turn)
 
-        state[cell.x][cell.y] = turn_max
 
-        if (isInTerminalState(state) || searchDepth == depthCount) {
-            var u = utilityCalculator.calculate(state,cell,turn_max )
-            println("MaxValue => $cell    || utility :  $u")
-            return u
+        // if game is not finished play the move
+        state.play(cell, turn, boardCalculator)
+
+        val opponent: Side = turn.flip()
+        var bestPoint = Double.MIN_VALUE
+        boardCalculator.availableCells(state, opponent).forEach {
+            bestPoint = max(bestPoint, minValue(state.copy(), boundary, it, opponent, depth + 1))
+            if (bestPoint >= boundary[1]) return bestPoint // if bestPoint >= beta return bestPoint
+            boundary[0] = max(boundary[0], bestPoint) // alpha = max(alpha,bestPoint)
         }
 
-        this.depthCount++
-        var v = boundary[1]
-        val availableMoves = boardCalculator.availableCells(boardCalculator.copy(state), turn_max)
-        availableMoves.forEach {
 
-            val minVal = minValue(boardCalculator.copy(state), boundary, it, Opponent)
-            v = max(v, minVal)
-
-            if (v >= boundary[1]) {
-                return v
-            }
-            boundary[0] = max(boundary[0], v)
-        }
-        return v
+        return bestPoint
     }
 
-    fun minValue(
+    private fun minValue(
         state: Array<Array<Side?>>,
-        boundary: Array<Double>,
+        boundary: DoubleArray,
         cell: Cell,
-        turn_min: Side
+        turn: Side,
+        depth: Int
     ): Double {
 
-        var Opponent: Side = turn_min.flip()
+        if (isTerminalState(state, depth))
+            return utilityCalculator.calculate(state, cell, turn)
 
-        state[cell.x][cell.y] = turn_min
-        if (isInTerminalState(state) || searchDepth == depthCount) {
-            var u = utilityCalculator.calculate(state,cell,turn_min )
-            println("MinValue => $cell    || utility :  $u")
-            return u
+        // if game is not finished play the move
+        state.play(cell, turn, boardCalculator)
+
+        val opponent: Side = turn.flip()
+        var bestPoint = Double.MAX_VALUE
+        boardCalculator.availableCells(state, opponent).forEach {
+            bestPoint = min(bestPoint, maxValue(state.copy(), boundary, it, opponent, depth + 1))
+            if (bestPoint <= boundary[0]) return bestPoint // if bestPoint <= alpha return bestPoint
+            boundary[1] = min(boundary[1], bestPoint) // alpha = min(beta,bestPoint)
         }
-        this.depthCount++
 
-        var v = boundary[0]
-        val availableMoves = boardCalculator.availableCells(boardCalculator.copy(state), turn_min)
-        availableMoves.forEach {
-
-            val maxVal = maxValue(boardCalculator.copy(state), boundary, it , Opponent)
-            v = min(v, maxVal)
-
-
-            if (v <= boundary[0]) {
-                return v
-            }
-            boundary[1] = min(boundary[1], v)
-        }
-        return v
+        return bestPoint
     }
 
-    fun isInTerminalState(state: Array<Array<Side?>>): Boolean {
-        var counter = 0
-        state.forEach { i ->
-            i.forEach { j ->
-                if (j != null)
-                    counter++
-            }
-        }
-        return counter >= 63
+    private fun isTerminalState(state: Array<Array<Side?>>, depth: Int): Boolean {
+        return depth == MAX_DEPTH || state.isFull()
     }
 }
