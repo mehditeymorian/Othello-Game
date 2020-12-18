92c37a5d831af5977c6d3be85a8afcc51fe580b4
Mehdi <mehditeymorian322@gmail.com>
2020-12-18 23:29:08 +0330

    Add helper functions

--- src/main/kotlin/game/Cell.kt
+++ src/main/kotlin/game/Cell.kt
@@ -1,3 +1,9 @@
 package game
 
-data class Cell(val x: Int,val y: Int)
+data class Cell(val x: Int,val y: Int) {
+
+
+    fun isValid(): Boolean {
+        return x in 0..7 && y in 0..7
+    }
+}
--- src/main/kotlin/game/Utils.kt
+++ src/main/kotlin/game/Utils.kt
@@ -11,7 +11,7 @@ fun Array<Array<Side?>>.copy(): Array<Array<Side?>> {
 
 fun Array<Array<Side?>>.play(cell: Cell, turn: Side, calculator: BoardCalculator) {
     this[cell.x][cell.y] = turn // put disk
-    calculator.flipCellsAfterMove(this,cell).forEach {
+    calculator.flipCellsAfterMove(this, cell).forEach {
         this[it.x][it.y] = this[it.x][it.y]?.flip()
     } // flip disks
 }
@@ -24,3 +24,50 @@ fun Array<Array<Side?>>.isFull(): Boolean {
 
     return true
 }
+
+fun Array<Array<Side?>>.isFrontier(cell: Cell): Boolean {
+    val (x, y) = cell
+
+    listOf(
+        Cell(x - 1, y - 1),
+        Cell(x - 1, y),
+        Cell(x - 1, y + 1),
+        Cell(x, y - 1),
+        Cell(x, y + 1),
+        Cell(x + 1, y - 1),
+        Cell(x + 1, y),
+        Cell(x + 1, y + 1),
+    ).forEach {
+        if (it.isValid() && getOrNull(it.x, it.y) == null) return true
+    }
+
+
+
+
+    return false
+}
+
+fun Array<Array<Side?>>.getOrNull(x: Int, y: Int): Side? {
+    return this.getOrNull(x)?.getOrNull(y)
+}
+
+fun Array<Array<Side?>>.leftMoves(): Int {
+    var leftMoves = 0
+    for (x in this)
+        for (y in x)
+            if (y == null) leftMoves++
+
+    return leftMoves
+}
+
+fun Array<Array<Side?>>.countDisks(): Pair<Int, Int> {
+    var whiteDisks = 0
+    var blackDisks = 0
+    for (x in this)
+        for (y in x)
+            if (y == Side.WHITE) whiteDisks++
+            else if (y == Side.BLACK) blackDisks++
+
+    return Pair(whiteDisks, blackDisks)
+}
+
