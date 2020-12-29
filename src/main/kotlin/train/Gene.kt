d005711e32d877d22f7573935285e94da67c114a
Mehdi <mehditeymorian322@gmail.com>
2020-12-29 18:06:11 +0330

    add id to gene

--- src/main/kotlin/train/Gene.kt
+++ src/main/kotlin/train/Gene.kt
@@ -1,5 +1,5 @@
 package train
 
-data class Gene(val generation: Int, val weights: FloatArray){
+data class Gene(val id: Int,val generation: Int, val weights: FloatArray) {
     var fitness: Int = 0
 }
