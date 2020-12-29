2ad5cf52dfb98b589c2ecc93adeacddf6ade3c4e
Haniye Mollaei <HaniyeMolaei1378@gmail.com>
2020-12-29 20:21:48 +0330

    change weight type

--- src/main/kotlin/train/Gene.kt
+++ src/main/kotlin/train/Gene.kt
@@ -1,5 +1,5 @@
 package train
 
-data class Gene(val id: Int,val generation: Int, val weights: FloatArray) {
+data class Gene(val id: Int, val generation: Int, val weights: Array<Double?>) {
     var fitness: Int = 0
 }
