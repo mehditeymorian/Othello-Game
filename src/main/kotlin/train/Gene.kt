e09294f3e005f78441a140d0adeec7033cc55be3
Haniye Mollaei <HaniyeMolaei1378@gmail.com>
2020-12-29 20:25:27 +0330

    change weight type to Double

--- src/main/kotlin/train/Gene.kt
+++ src/main/kotlin/train/Gene.kt
@@ -1,5 +1,5 @@
 package train
 
-data class Gene(val id: Int, val generation: Int, val weights: Array<Double?>) {
+data class Gene(val id: Int, val generation: Int, val weights: Array<Double>) {
     var fitness: Int = 0
 }
