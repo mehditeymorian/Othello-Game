992be710e063bc9cef5e9c21bdcfca5e4e391912
Mehdi <mehditeymorian322@gmail.com>
2020-12-31 22:17:09 +0330

    add toString for gene

--- src/main/kotlin/train/Gene.kt
+++ src/main/kotlin/train/Gene.kt
@@ -6,4 +6,8 @@ import java.util.concurrent.atomic.AtomicInteger
 data class Gene(val id: Int, val generation: Int, val weights: DoubleArray) {
     var fitness: AtomicInteger = AtomicInteger(0)
     var selected: Boolean = false
+
+    override fun toString(): String {
+        return "Gene($id)-> ${weights.joinToString(" , ") { String.format("%.3f", it) }}"
+    }
 }
