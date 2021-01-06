5e1d47d2ef5c8a0e2a4888552808c0a5ad0d33c9
Mehdi <mehditeymorian322@gmail.com>
2021-01-06 23:03:55 +0330

    Convert fitness to atomic reference of type double

--- src/main/kotlin/train/Gene.kt
+++ src/main/kotlin/train/Gene.kt
@@ -1,10 +1,10 @@
 package train
 
-import java.util.concurrent.atomic.AtomicInteger
+import java.util.concurrent.atomic.AtomicReference
 
 @Suppress("ArrayInDataClass")
 data class Gene(val id: Int, val generation: Int, val weights: DoubleArray) {
-    var fitness: AtomicInteger = AtomicInteger(0)
+    var fitness = AtomicReference(0.0)
     var selected: Boolean = false
 
     override fun toString(): String {
